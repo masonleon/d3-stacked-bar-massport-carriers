f94352a9795b59597446228bd93f6c9f313cf51a
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-11-03 16:06:40 -0400

    cleanup

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 // feel free to change the preset attributes as you see fit
 const massportCarrierAllianceCsv = './data/MASSPORT_SSL_Alliance_By_Quarter.csv';
 
-const seaIntelligenceCarrierScheduleReliabilityCsv = './data/20210129_-_Sea-Intelligence_GLP_Press_Release_Charts_-_January_2021_global_scheduled_reliability2.csv';
-
 d3.csv(massportCarrierAllianceCsv, d3.autoType())
   .then(barChart);
 
@@ -154,79 +152,7 @@ function barChart(data){
       .attr("transform", `translate(${(width + margin.left + margin.right)/2},20)`)
       .attr("text-anchor", "middle")
       .style("font-weight", 600)
-      .html("MassPort Container Carriers by Alliance FY2014 - Present")
-}
-
-
-let parseDate = d3.timeParse('%m/%d/%Y');
-
-d3.csv(seaIntelligenceCarrierScheduleReliabilityCsv, function(d) {
-  return {
-    date: parseDate(d.Month),
-    reliability: +d.Reliability
-  };
-}).then(lineChart);
-
-
-function lineChart(data){
-  console.log(data);
-
-  let maxDate  = d3.max(data, function(d){return d.date; });
-  let minDate  = d3.min(data, function(d){return d.date; });
-  let maxReliability = d3.max(data, function(d){return d.reliability;});
-  
-  console.log(maxDate, minDate, maxReliability);
-
-  let xScale = d3.scaleTime()
-      .domain([minDate, maxDate])
-      .range([0, width - margin.left - margin.right]);
-
-  let yScale = d3.scaleLinear()
-      .domain([0, maxReliability])
-      .range([height - margin.bottom - margin.top, 0]);
-
-  let xAxis = d3.axisBottom(xScale);
-  svg2.append('g')
-      .attr('class', 'x axis')
-      .attr("transform", `translate(${margin.right + margin.left},${height - margin.bottom})`)      
-      .call(xAxis);
-
-  svg2.append("g")
-    .selectAll("text")  
-    .style("text-anchor", "end")
-    .attr("transform", "rotate(-65)");
-
-  // https://www.d3-graph-gallery.com/graph/custom_axis.html
-  // X axis label
-  svg2.append("text")
-    .attr('class', 'axis-label')
-    .attr("x", margin.left + (width - margin.left - margin.right) / 2)
-    .attr("y", height + 50)
-    .text("Month");
-
-  let yAxis = d3.axisLeft(yScale);
-  svg2.append('g')
-      .attr('class', 'y axis')
-      .attr("transform", `translate(${margin.right + margin.left},${margin.top})`)      
-      .call(yAxis);
-
-    // Y axis label
-    svg2.append("text")
-      .attr('class', 'axis-label')
-      .attr('transform', 'rotate(-90)')
-      .attr('x', -(margin.top + (height - margin.top - margin.bottom)))
-      .attr('y', 20) // Relative to the y axis.
-      .text("2020 Global Carrier Schedule Reliability");
-      
-  let line = d3.line()
-      .x(function(d){return xScale(d.date);})    
-      .y(function(d){return yScale(d.reliability);})
-
-  svg2.append('path')
-      .attr('d', line(data))
-      .attr("transform", `translate(${margin.right + margin.left},${margin.top})`)      
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
+      .html("MassPort Container Carriers by Alliance FY2014 - FY2020")
 }
 
 let margin = {
@@ -240,14 +166,6 @@ let margin = {
 
 // first visualization
 let svg1 = d3.select('#vis1')
-  .append('svg')
-  .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
-  .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
-  .style('background-color', '#ccc') // change the background color to light gray
-  .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-
-// second visualization
-let svg2 = d3.select('#vis2')
   .append('svg')
   .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
   .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
